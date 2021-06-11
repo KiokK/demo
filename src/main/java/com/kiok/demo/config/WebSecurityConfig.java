e92814a3d5eb052dfa64148e4fc873718b78e147
KiokK <kiok.ko1811@gmail.com>
2021-06-11 20:53:08 +0300

    +admin and redact-user

--- src/main/java/com/kiok/demo/config/WebSecurityConfig.java
+++ src/main/java/com/kiok/demo/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.kiok.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // чтоб работало @PreAuthorize("hasAuthority('ADMIN')")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
---
+++ src/main/java/com/kiok/demo/controllers/UserController.java
@@ -0,0 +1,65 @@
+package com.kiok.demo.controllers;
+
+import com.kiok.demo.models.Message;
+import com.kiok.demo.models.Role;
+import com.kiok.demo.models.User;
+import com.kiok.demo.repo.UserRepos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
+public class UserController {
+    @Autowired
+    public UserRepos userRepos;
+
+    @GetMapping
+    public String userList(Model model){
+        Iterable<User> all = userRepos.findAll();
+
+        model.addAttribute("userList", all);
+        return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEdit(@PathVariable User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(
+            @RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user) {
+        user.setUsername(username);
+
+        //из перечисления в строку
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepos.save(user);
+
+        return "redirect:/user";
+    }
+}
